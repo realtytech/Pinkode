3b4d2b938be448bc11a3f588394b3dfad749462d
abcf98 <amanframewala@gmail.com>
2021-01-28 16:22:54 +0530

    SRDs Setup for SoBo

--- projects/south-bombay/premium/js/main.js
+++ projects/south-bombay/premium/js/main.js
@@ -46,6 +46,7 @@ $("#leadForm-popup").submit(function (e) {
     var name = $("#name-popup").val();
     var email = $("#email-popup").val();
     var mobile = $("#mobile-popup").val();
+    var project = $("#project-popup :selected").text();
     if (name == "") {
         alert('Please enter your name');
         return;
@@ -73,7 +74,7 @@ $("#leadForm-popup").submit(function (e) {
     }
 
 
-    // var srd = selectSRD(utm_source, utm_campaign);
+    var srd = selectSRD(utm_source, utm_campaign);
     var srd = queryParameter('srd', currentUrl);
     if (!srd) srd = '5faa0bce4443ae474efa311d';
 
@@ -91,7 +92,7 @@ $("#leadForm-popup").submit(function (e) {
         success: function (response) {
             console.log(JSON.parse(response));
             storeLeadInDB(name, email, mobile, JSON.stringify(response));
-            setTimeout(function redirect_response(){window.location.href = "response.html";}, 1000)
+            setTimeout(function redirect_response() { window.location.href = "response.html"; }, 1000)
         },
         error: function (xhr) {
             //Do Something to handle error
@@ -127,6 +128,7 @@ $("#leadForm").submit(function (e) {
     var name = $("#name").val();
     var email = $("#email").val();
     var mobile = $("#mobile").val();
+    var project = $("#project :selected").text();
     if (name == "") {
         alert('Please enter your name');
         return;
@@ -154,7 +156,7 @@ $("#leadForm").submit(function (e) {
     }
 
 
-    // var srd = selectSRD(utm_source, utm_campaign);
+    var srd = selectSRD(queryParameter('souce', currentUrl), queryParameter('sub_source', currentUrl),project);
     var srd = queryParameter('srd', currentUrl);
     if (!srd) srd = '5faa0bce4443ae474efa311d';
 
@@ -171,8 +173,8 @@ $("#leadForm").submit(function (e) {
         },
         success: function (response) {
             console.log(JSON.parse(response));
-            storeLeadInDB(name, email, mobile, JSON.stringify(response),formName);
-            setTimeout(function redirect_response(){window.location.href = "response.html";}, 1000)
+            storeLeadInDB(name, email, mobile, JSON.stringify(response), formName);
+            setTimeout(function redirect_response() { window.location.href = "response.html"; }, 1000)
         },
         error: function (xhr) {
             //Do Something to handle error
@@ -250,7 +252,7 @@ $("#leadFormMobile").submit(function (e) {
         success: function (response) {
             console.log(JSON.parse(response));
             storeLeadInDB(name, email, mobile, JSON.stringify(response));
-            setTimeout(function redirect_response(){window.location.href = "response.html";}, 1000)
+            setTimeout(function redirect_response() { window.location.href = "response.html"; }, 1000)
         },
         error: function (xhr) {
             //Do Something to handle error
@@ -353,7 +355,7 @@ function verifyOtpAPI() {
     });
 }
 
-function storeLeadInDB(name, email, mobile, response,formName) {
+function storeLeadInDB(name, email, mobile, response, formName) {
     var currentUrl = window.location.href;
     var utm_source = queryParameter('utm_source', currentUrl);
     var utm_medium = queryParameter('utm_medium', currentUrl)
@@ -393,7 +395,7 @@ function storeLeadInDB(name, email, mobile, response,formName) {
         "gclid": gclid,
         "fbclid": fbclid,
         "response": response,
-        "formName":formName
+        "formName": formName
 
     }
     const formURL = 'https://dj2kxzt125.execute-api.ap-south-1.amazonaws.com/Prod/submitForm';
@@ -440,15 +442,106 @@ function storeLeadInDB(name, email, mobile, response,formName) {
     //     }
     // });
 }
-function selectSRD(utm_source, utm_campaign,project) {
+function selectSRD(source, sub_source, project) {
+    var project_srd = {
+
+        "Bombay Realty - Island City Center": "6012943da6bbc90bcb739a40",
+        "Rustomjee Crown": "60129457a6bbc90394be333f",
+        "K Raheja Corp - Vivarea": "6012948ea6bbc905e8df5c8e",
+        "Lodha Park": "601294a4a6bbc90394be339e"
+    }
+
+    
+    if (!source){
+        return project_srd[project];
+    }
+
+        var srd = {
+            "Bombay Realty - Island City Center": {
+
+                "Google_Search": {
+                    "Brand": "601288584443ae7167c9d85e",
+                    "Offer_Specific": "601288684443ae6a4756e94f",
+                    "Generic": "601288764443ae7167c9d875",
+                    "Competition": "601288834443ae6a4756e966"
+                },
+                "Progammatic": {
+                    "Intent": "601288924443ae77a6f25bd1",
+                    "Affinity": "601288a24443ae66de17fb92",
+                    "Remarketing": "601288ad4443ae384113e0e7"
+                },
+                "Facebook": {
+                    "Lead_Gen_Property_Interest": "601288c74443ae6681180070",
+                    "Lead_Gen_Competitor": "601288db4443ae6bf1416c92"
+                }
+            },
+            "Rustomjee Crown": {
+
+                "Google_Search": {
+                    "Brand": "601288eb4443ae7167c9d8c8",
+                    "Offer_Specific": "601288f74443ae66de17fbd1",
+                    "Generic": "601289044443ae6a4756e9b9",
+                    "Competition": "601289104443ae4b0cab5958"
+                },
+                "Progammatic": {
+                    "Intent": "601289fe4443ae4b0cab59e5",
+                    "Affinity": "60128a104443ae73d497829d",
+                    "Remarketing": "60128a1f4443ae4b0cab59f5"
+                },
+                "Facebook": {
+                    "Lead_Gen_Property_Interest": "60128a354443ae668118016f",
+                    "Lead_Gen_Competitor": "60128a434443ae6bf1416d44"
+                }
+            },
+
+            "K Raheja Corp - Vivarea": {
+
+                "Google_Search": {
+                    "Brand": "60128a744443ae66de17fcbf",
+                    "Offer_Specific": "60128a854443ae66811801a4",
+                    "Generic": "60128a944443ae66811801a9",
+                    "Competition": "60128aa94443ae6a4756eaa6"
+                },
+                "Progammatic": {
+                    "Intent": "60128ab74443ae3fb9451593",
+                    "Affinity": "60128ac54443ae384113e245",
+                    "Remarketing": "60128ad24443ae384113e257"
+                },
+                "Facebook": {
+                    "Lead_Gen_Property_Interest": "60128adf4443ae4b0cab5a6b",
+                    "Lead_Gen_Competitor": "60128af04443ae66811801f1"
+                }
+            },
+
+            "Lodha Park": {
+
+                "Google_Search": {
+                    "Brand": "60128b064443ae384113e276",
+                    "Offer_Specific": "60128b174443ae3fb94515ec",
+                    "Generic": "60128b284443ae7167c9d9c8",
+                    "Competition": "60128b3f4443ae73d4978325"
+                },
+                "Progammatic": {
+                    "Intent": "60128b514443ae66de17fd3d",
+                    "Affinity": "60128c884443ae3fb94516e9",
+                    "Remarketing": "60128c9b4443ae384113e386"
+                },
+                "Facebook": {
+                    "Lead_Gen_Property_Interest": "60128cac4443ae66de17fe08",
+                    "Lead_Gen_Competitor": "60128cbe4443ae6bf1416e89"
+                }
+            }
+        }
+
+    return srd[project][source][sub_source];
 
     var srd = '';
 
     var google_srd = {
-        "park":"60114aaec825613e5e6c040b",
+        "park": "60114aaec825613e5e6c040b",
         "icc": "60114ac6c82561218b838325",
         "vivarea": "60114adfc825612a493e5989",
-        "crown":"60114bedc82561251b6cbb77"
+        "crown": "60114bedc82561251b6cbb77"
     }
 
     // Google srd
@@ -576,9 +669,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-function showModal(type,project) {
+function showModal(type, project) {
     if (type == 'project') {
-        
+
         $("#sellModal").modal("show");
         $('#project-popup').prop('selectedIndex', project);
         localStorage.removeItem('myTimestamp');
@@ -614,22 +707,22 @@ window.onscroll = function (e) {
 }
 
 var banner = document.getElementById('banner-scroll');
-banner.onmouseover = function(e) {
-    setTimeout(() => { $( 'html, body' ).stop().animate({scrollTop:$('.desktop-only .container-fluid').offset().top}, '1500'); }, 2000);
+banner.onmouseover = function (e) {
+    setTimeout(() => { $('html, body').stop().animate({ scrollTop: $('.desktop-only .container-fluid').offset().top }, '1500'); }, 2000);
 
 
-    
+
 
     // $(window).scrollTop($('.desktop-only .container-fluid').offset().top);
-    banner.onmouseover = function(){return;}
+    banner.onmouseover = function () { return; }
 };
 
-document.onload = function(e) {
-    setTimeout(() => { $( 'html, body' ).stop().animate({scrollTop:$('.desktop-only .container-fluid').offset().top}, '1500'); }, 5000);
+document.onload = function (e) {
+    setTimeout(() => { $('html, body').stop().animate({ scrollTop: $('.desktop-only .container-fluid').offset().top }, '1500'); }, 5000);
 
 
-    
+
 
     // $(window).scrollTop($('.desktop-only .container-fluid').offset().top);
-    banner.onmouseover = function(){return;}
+    banner.onmouseover = function () { return; }
 };
